ef9512524a6648c76cbcd4a6cfe2c58e64815991
Robert E. Lee <robert@loveathome.us>
2026-03-20 13:27:00 -0700

    v0.5.0 final: sockpath + port + PostgREST fixes

--- Formula/unicornscan.rb
+++ Formula/unicornscan.rb
@@ -16,7 +16,7 @@ class Unicornscan < Formula
   desc "Asynchronous stateless TCP/UDP network scanner with web UI"
   homepage "https://github.com/robertelee78/unicornscan"
   url "https://github.com/robertelee78/unicornscan/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "36ae088897f904ff383101f3a744a143bee52628e5219463a6425d2591036a78"
+  sha256 "e421875ab483a23dfb4fa749d1a36d3401b497dc1558956a8af2cfc920e770f1"
   license "GPL-2.0-or-later"
   head "https://github.com/robertelee78/unicornscan.git", branch: "main"
 
