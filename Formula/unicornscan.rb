04307d3444e8fb9d8609f4ec1ac83acec0f24300
robertelee78 <63057704+robertelee78@users.noreply.github.com>
2026-03-26 10:43:24 -0700

    Update formula sha256 for re-tagged v0.4.52

--- Formula/unicornscan.rb
+++ Formula/unicornscan.rb
@@ -16,7 +16,7 @@ class Unicornscan < Formula
   desc "Asynchronous stateless TCP/UDP network scanner with web UI"
   homepage "https://github.com/robertelee78/unicornscan"
   url "https://github.com/robertelee78/unicornscan/archive/refs/tags/v0.4.52.tar.gz"
-  sha256 "5bdbec4041fff6659171ea689de8e791d6ac6b02951346f61cccec0e2c4766e3"
+  sha256 "f405314e5668ecc007835f413e5ff8afa4b374d57edaf43fdec8f152149b7a76"
   license "GPL-2.0-or-later"
   head "https://github.com/robertelee78/unicornscan.git", branch: "main"
 
